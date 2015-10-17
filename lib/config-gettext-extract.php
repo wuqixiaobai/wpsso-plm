915ced62edb5aa8f2bb432e396925a3cd8ced424
JS Morisset <jsm@surniaulula.com>
2015-10-17 03:06:12 -0400

    v1.3.8

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// an extract of translatable text strings from the static config array
+// extract of translatable text strings from static config array(s)
 _x( 'Place / Location Meta', 'lib file description', 'wpsso-plm' );
 _x( 'WPSSO extension to provide Facebook / Open Graph "Location" and Pinterest "Place" Rich Pin meta tags.', 'plugin description', 'wpsso-plm' );
 ?>
