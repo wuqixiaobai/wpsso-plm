868b88729d8fddba2d2e46dca3b610dc4c4175a7
JS Morisset <jsm@surniaulula.com>
2016-01-13 09:31:16 -0600

    v1.3.11 author name change

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -5,7 +5,7 @@
  * Text Domain: wpsso-plm
  * Domain Path: /languages
  * Plugin URI: http://surniaulula.com/extend/plugins/wpsso-plm/
- * Author: Jean-Sebastien Morisset
+ * Author: JS Morisset
  * Author URI: http://surniaulula.com/
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl.txt
