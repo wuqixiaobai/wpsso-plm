d57f6a9171750a06c49ebaadc426a8ace2c3497c
JS Morisset <jsm@surniaulula.com>
2016-03-20 17:27:06 -0400

    v1.5.0

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,6 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
 // extract of translatable text strings from static config array(s)
-_x( '', 'option value', 'wpsso-plm' );
 _x( 'Corporate Contacts', 'lib file description', 'wpsso-plm' );
 _x( 'Geographic', 'option value', 'wpsso-plm' );
 _x( 'Place / Location Meta', 'lib file description', 'wpsso-plm' );
