f35bec1fd5fc3fe0d76bd00707ec487b21cf3125
JS Morisset <jsm@surniaulula.com>
2016-10-14 21:21:46 -0300

    v2.2.0-1

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business + Local SEO meta tags.
  * Requires At Least: 3.1
  * Tested Up To: 4.6.1
- * Version: 2.2.0-rc1
+ * Version: 2.2.0-1
  * 
  * Version Numbers: {major}.{minor}.{bugfix}-{stage}{level}
  *
@@ -40,7 +40,7 @@ if ( ! class_exists( 'WpssoPlm' ) ) {
 		private static $instance = null;
 		private static $req_short = 'WPSSO';
 		private static $req_name = 'WordPress Social Sharing Optimization (WPSSO)';
-		private static $req_min_version = '3.36.0-rc1';
+		private static $req_min_version = '3.36.0-1';
 		private static $req_has_min_ver = true;
 
 		public static function &get_instance() {
