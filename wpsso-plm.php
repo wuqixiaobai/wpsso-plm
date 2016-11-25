d025005718b1de3cf92b0c41cf9f6d86f2be9b80
JS Morisset <jsm@surniaulula.com>
2016-11-25 11:28:13 -0800

    v2.2.4-1

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business + Local SEO meta tags.
  * Requires At Least: 3.7
  * Tested Up To: 4.6.1
- * Version: 2.2.4-rc2
+ * Version: 2.2.4-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
  *
