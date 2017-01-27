cc0bcad55c9fb647b955f14a297159c14c4d38cc
JS Morisset <jsm@surniaulula.com>
2017-01-26 17:03:06 -0800

    v2.2.6-1

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to provide Pinterest Place, Facebook / Open Graph Location, Schema Local Business + Local SEO meta tags.
  * Requires At Least: 3.7
- * Tested Up To: 4.7.1
+ * Tested Up To: 4.7.2
  * Version: 2.2.6-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
