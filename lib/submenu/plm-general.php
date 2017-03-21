d653557cfb7219d333ce4896baf757f09a688da4
JS Morisset <jsm@surniaulula.com>
2017-03-21 07:20:19 -0700

    v2.2.10-dev1

--- lib/submenu/plm-general.php
+++ lib/submenu/plm-general.php
@@ -97,8 +97,8 @@ if ( ! class_exists( 'WpssoPlmSubmenuPlmGeneral' ) && class_exists( 'WpssoAdmin'
 					list( $first_num, $last_num, $next_num ) = SucomUtil::get_first_last_next_nums( $this->form->__address_names );
 					$this->form->__address_names[$next_num] = WpssoPlmConfig::$cf['form']['plm_addr_select']['new'];
 					$this->form->__all_types = $this->p->schema->get_schema_types_array( false );	// $flatten = false
-					$this->form->__business_types = $this->p->schema->get_schema_types_select( $this->form->__all_types['thing']['place']['local.business'],
-						false );	// $add_none = false
+					$this->form->__business_types = $this->p->schema->get_schema_types_select(
+						$this->form->__all_types['thing']['place']['local.business'], false );	// $add_none = false
 					$this->form->__half_hours = SucomUtil::get_hours_range( 0, 86400, 60 * 30, '' );
 					$this->form->defaults['plm_addr_id'] = $first_num;	// set default value
 
