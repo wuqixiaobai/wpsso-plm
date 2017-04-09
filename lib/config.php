47c1d16b1978f4224310bd3f07e2b6e08e022fe6
JS Morisset <jsm@surniaulula.com>
2017-04-09 09:27:58 +0200

    v2.2.12

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoplm' => array(
-					'version' => '2.2.12',	// plugin version
+					'version' => '2.2.12',		// plugin version
 					'opt_version' => '15',		// increment when changing default options
 					'short' => 'WPSSO PLM',		// short plugin name
 					'name' => 'WPSSO Place / Location and Local Business Meta (WPSSO PLM)',
