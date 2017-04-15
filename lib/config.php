d7cf98eb6efce49e006e5d4446275ea31a813208
JS Morisset <jsm@surniaulula.com>
2017-04-15 09:07:57 +0200

    v2.2.13-rc.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoplm' => array(
-					'version' => '2.2.13-rc.1',		// plugin version
+					'version' => '2.2.13-rc.2',		// plugin version
 					'opt_version' => '15',		// increment when changing default options
 					'short' => 'WPSSO PLM',		// short plugin name
 					'name' => 'WPSSO Place / Location and Local Business Meta (WPSSO PLM)',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.40.13-rc.1',
+						'min_version' => '3.40.13-rc.2',
 					),
 					'img' => array(
 						'icons' => array(
