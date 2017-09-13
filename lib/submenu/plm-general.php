60d3d7eeffdf0e9c6e601b6a28da576521954d71
JS Morisset <jsm@surniaulula.com>
2017-09-13 12:07:33 -0400

    v2.3.3

--- lib/submenu/plm-general.php
+++ lib/submenu/plm-general.php
@@ -90,7 +90,7 @@ if ( ! class_exists( 'WpssoPlmSubmenuPlmGeneral' ) && class_exists( 'WpssoAdmin'
 					}
 
 					$add_to_checkboxes = '';
-					foreach ( $this->p->util->get_post_types( 'object' ) as $pt ) {
+					foreach ( $this->p->util->get_post_types( 'objects' ) as $pt ) {
 						$add_to_checkboxes .= '<p>'.( $aop ? $this->form->get_checkbox( 'plm_add_to_'.$pt->name ) :
 							$this->form->get_no_checkbox( 'plm_add_to_'.$pt->name ) ).
 							' '.$pt->label.( empty( $pt->description ) ? '' : ' ('.$pt->description.')' ).'</p>';
